42cdeff616118b4ea671754c0f01576545e5a50b
Lantern <c-w1sp@protonmail.com>
2025-01-11 17:09:40 -0500

    shit
    not like that

--- Binary File Test/Key.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-#should be the same as Tileset Source ID
-#temp is manual
-const TILESET_IDS := {
-	"res://Testing/MiniProto.tres":0
-}
-
-@export var tilesets : Array[String]
--- Binary File Test/README.md
+++
@@ -1,2 +0,0 @@
-# Tilemap Binary Compressor V0.1.0
-[desc]
